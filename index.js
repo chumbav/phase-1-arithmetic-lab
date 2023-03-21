f12d80a0ff5a5e91dcb7bdfd69a768ac12e14950
Victor <victor.chumba@student.moringaschool.com>
2023-03-21 14:28:34 +0300

    Completed assigment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+const num1=31;
+const num2=2;
 
-// Write your code here
+const multiply = Math.floor(num1*num2);
+
+
+let random = Math.floor(Math.random(0)*100)+1;
+
+const num3=40
+const num4=6
+
+ let mod = num3 % num4;
+  
+
+
+ let max =Math.max(20);
+  
